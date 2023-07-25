3297f67560ee1b3d5615f6be81d3c6321e21f457
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-07-25 20:04:03 +0500

    Добавил решение по задачам 11-16

---
+++ Task012_Multiple_XY/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача № 12
+Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если число 2 не 
+кратно числу 1, то программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно
+*/
+Console.Clear();
+
+Console.WriteLine("Введите первое число ");
+string userInput1 = Console.ReadLine() ?? ""; 
+int a = int.Parse(userInput1); 
+
+Console.WriteLine("Введите второе число ");
+string userInput2 = Console.ReadLine() ?? ""; 
+int b = int.Parse(userInput2);
+
+if (a % b == 0)
+{
+    Console.WriteLine($"{a}, {b} -> кратно");
+}
+else 
+{
+    Console.WriteLine($"{a}, {b} -> не кратно, остаток {a % b}");
+}
+
---
+++ Task016_A_square_B/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+Задача № 16. Напишите программу, которая принимает на вход два числа 
+и проверяет, является ли одно число квадратом другого.
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет 
+*/
+Console.Clear();
+
+Console.WriteLine("Введите первое число ");
+string userInput1 = Console.ReadLine() ?? ""; 
+int a = int.Parse(userInput1); 
+
+Console.WriteLine("Введите второе число ");
+string userInput2 = Console.ReadLine() ?? ""; 
+int b = int.Parse(userInput2);
+
+if (b == a*a || a == b*b)
+{
+    Console.WriteLine($"{a}, {b} -> да");
+}
+else 
+{
+    Console.WriteLine($"{a}, {b} -> нет");
+}
