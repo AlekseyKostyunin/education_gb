357552e1913038494fcb081c53326f61131a8c22
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-07-28 18:29:41 +0500

    Добавил решение по задаче № 18

---
+++ Task018_N4_XY/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+int InputNumber(string message, string errorMessage)
+{
+    while(true)
+    {
+        try{
+            Console.Write(message);
+            int a = int.Parse(Console.ReadLine() ?? "");
+            return a;
+        }
+        catch
+        {
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
+
+string OutSector (int number)
+{
+    if(number == 1)
+    {
+        return "X > 0, Y > 0";
+    }
+    else if(number == 2)
+    {
+         return "X < 0, Y > 0";
+    }
+    else if(number == 3)
+    {
+        return "X < 0, Y < 0";
+    }
+    else if(number == 4)
+    {
+        return "X > 0, Y < 0";
+    }
+    else
+    {
+        return "Ошибка! Введите корректный номер четверти";
+    }
+}
+
+int numberN = InputNumber("Введите номер четверти: ", "Ошибка! Некорректный номер");
+string mes = OutSector(numberN);
+Console.WriteLine(mes);
