80054b9eaf3ea1912cb779ff4554d63b8c328d9f
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-08-04 18:15:03 +0500

    добавил решение задач 32, 33, 35, 37

---
+++ Task033_Search_N_Array/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 33:
+Задайте массив. Напишите программу, которая
+определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3]--> нет
+3; массив [6, 7, 19, 345, 3] --> да
+*/
+
+Console.Clear();
+
+int[] array = new int[5] {6, 7, 19, 345, 3};
+
+int a = GetNumberFromUser("Введите число: ", "Ошибка! Введите целое число.");
+bool result = FindNumber(a, array); 
+
+Console.WriteLine($"{a}: массив {String.Join(" ", array)} -> {(result ? "да" : "нет")}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+bool FindNumber(int a, int[] array)
+{
+    foreach (int el in array)
+    {
+        if (el == a) return true;
+    }
+    return false;
+}
---
+++ Task037_Product_Par_Array/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 37:
+Найдите произведение пар чисел в одномерном массиве.
+Парой считаем первый и последний элемент, второй и предпоследний
+и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5]--> 5 8 3
+[6 7 3 6]--> 36 21
+*/
+
+Console.Clear();
+
+int[] array = new int[4] {6, 7, 3, 6};
+Console.WriteLine(String.Join(" ", array));
+
+int[] arrnew = GetProductPar(array);
+Console.WriteLine(String.Join(" ", arrnew));
+
+int[] GetProductPar(int[] array)
+{
+    int s = array.Length / 2;
+    int j = array.Length - 1;
+    int[] arrnew = new int [s];
+    for (int i = 0; i < s; i++)
+    {
+        arrnew[i] = array[i] * array[j];
+        j--;
+    }
+    return arrnew;
+}
