90f91da0f593d99f455ce572505cd7130167d86b
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-08-04 19:23:00 +0500

    добавил решение задач 34, 36, 38

---
+++ Task036_Sum_el_nechet/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный
+случайными числами. Найдите сумму элементов, стоящих
+на нечётных позициях.
+[3, 7, 23, 12]--> 19
+[4, 6, 89, 6] --> 0
+*/
+
+Console.Clear();
+
+int [] array = GetArray(6, 1, 20);
+Console.WriteLine(String.Join(" ", array));
+
+int sum = GetSumOdd(array);
+Console.WriteLine($"Сумма элементов массива, стоящих на нечётных позициях, равна: {sum}");
+
+int GetSumOdd(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i % 2 == 1) sum += arr[i];
+    }
+    return sum;
+}
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
---
+++ Task038_Difference_min_max_array/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел.
+Найдите разницу между максимальным и минимальным
+элементов массива.
+[3 7 22 2 78] --> 76
+*/
+Console.Clear();
+
+double[] array = GetArray(6);
+Console.WriteLine(String.Join(" ", array));
+
+double dif = GetDifMaxMin(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна: {dif}");
+
+double [] GetArray(int size)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().NextDouble() * 10;
+    }
+    return res;
+}
+
+// Вычисляем разницу между максимальным и минимальным элементом массива
+double GetDifMaxMin(double[] arr)
+{
+    double max = GetMaxArray(arr);
+    double min = GetMinArray(arr);
+    return max - min;
+}
+
+// Определяем максимальный элемент
+double GetMaxArray(double[] array)
+{
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+// Определяем минимальный элемент
+double GetMinArray(double[] array)
+{
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
+}
