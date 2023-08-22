bf59f1c9c2fa45184aac401f3a87989bb3628768
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-08-22 18:11:41 +0500

    добавил решение задач 42, 43, 44, 45

---
+++ Task042_10_to_2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+
+Console.Clear();
+
+int a = 100;
+int[] result = Get2(a);
+Console.Write($"{a} -> ");
+Console.WriteLine(String.Join("", result));
+
+int[] Get2(int number)
+{
+    int temp = number;
+    int count = 0;
+    while(temp > 0)
+    {
+        temp = temp / 2;
+        count++;
+    }
+    int[] array = new int [count];
+    int i = array.Length - 1;
+    while(number > 0)
+    {
+        array[i] = number % 2;
+        number = number / 2;
+        i--;
+    }
+    return array;
+}
---
+++ Task044_Fibonachi/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+Console.Clear();
+int n = GetNumberFromUser("Введите число N больше 2: ", "Ошибка!");
+int[] result = GetN(n);
+Console.Write($"Если {n} -> ");
+Console.WriteLine(String.Join(" ", result));
+
+int[] GetN(int n)
+{
+    int[] res = new int[n];
+    res[0] = 0;
+    res[1] = 1;
+    for (int i = 2; i < res.Length; i++)
+    {
+        res[i] = res[i-1] + res[i-2];
+    } 
+    return res;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect && userNumber > 2)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
