41f048af920aaa121b9b9892e8168c44261b4419
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-08-22 18:09:39 +0500

    добавил решение задач 39, 40, 41

---
+++ Task040_Triangle_ABC/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 40: Напишите программу, которая принимает на вход три числа и 
+проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше 
+суммы двух других сторон.
+*/
+
+Console.Clear();
+
+int a = GetNumberFromUser("Введите длину первой стороны: ", "Ошибка! Введите целое число.");
+int b = GetNumberFromUser("Введите длину второй стороны: ", "Ошибка! Введите целое число.");
+int c = GetNumberFromUser("Введите длину третьей стороны: ", "Ошибка! Введите целое число.");
+
+bool result = Proverka(a, b, c);
+
+Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c} -> {(result ? "существует" : "не существует")} ");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+bool Proverka(int A, int B, int C)
+{
+    if ((A < B + C) && (B < A + C) && (C < B + A)) return true;
+    return false;
+}
---
+++ Task041_M_above_zero/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+
+int[] array = new int[5] {6, 7, 19, 345, 3};
+
+int a = GetNumberFromUser("Введите число: ", "Ошибка! Введите целое число.");
+
+Console.WriteLine($"{a}: Массив: {String.Join(" ", array)} -> {result}");
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
