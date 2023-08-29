66e2a4cb6fab81f8560240b25041a30f4f22d554
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-08-29 18:41:05 +0500

    добавил решение задач 54 (дз)

---
+++ Task054_SortArrMinI/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите количество столбцов массива: ");
+int colums = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine();
+
+int[,] array = GetArray(rows, colums, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+SortElementRows(array);
+PrintArray(array);
+
+void SortElementRows(int[,] inArray)
+{    
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            int maxPosition = j;
+            for (int k = j+1; k < inArray.GetLength(1); k++)
+            {
+                if(inArray[i,k] > inArray[i,maxPosition]) maxPosition = k;
+            } 
+            int temp =  inArray[i, j];
+            inArray[i, j] = inArray[i, maxPosition];
+            inArray[i, maxPosition] = temp; 
+        }
+    }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
