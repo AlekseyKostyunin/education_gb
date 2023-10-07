acbc75865695c49619781807dc54080b70ca98d5
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-10-07 20:06:08 +0500

    Добавил решение задачи 16 2 вариант

--- Python/Task16/main.py
+++ Python/Task16/main.py
@@ -18,3 +18,11 @@ for i in range(len(numbers)):
     if numbers[i] == x:
         count += 1
 print(count)
+
+# 2 вариант
+# n = int(input('Введите количество элементов массива (списка): '))
+# numbers = []
+# for i in range(n):
+#     numbers.append(int(input('Введите целое число: '))) 
+# x = int(input('Введите искомое число: '))
+# print(numbers.count(x))
