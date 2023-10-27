f778f4d81985b04c7de47ce22e8c9ce964f09cd5
AlekseyKostyunin <kostunin-ag@mail.ru>
2023-10-27 19:24:23 +0500

    Добавил решение задачи

--- Python/Task38/main.py
+++ Python/Task38/main.py
@@ -1,9 +1,18 @@
 '''Задача №38. Дополнить справочник возможностью копирования данных из одного файла в другой. 
 Пользователь вводит номер строки, которую необходимо перенести из одного файла в другой.
 '''
-
 phonebook = open('phonebook.txt', 'r')
+s=[]
 for line in phonebook:
-    print(line)
+    s.append(line.split(','))
+phonebook.close
 
-phonebook.close() 
+phonebook_copy = open('phonebook_copy.txt', 'a')
+
+n = int(input("Введите номер строки для копирования: "))
+
+if (n>len(s)):
+    print("Такой строки не существует")
+else:
+    str = ",".join(s[n-1])
+    phonebook_copy.writelines(f"{str}\n" )
