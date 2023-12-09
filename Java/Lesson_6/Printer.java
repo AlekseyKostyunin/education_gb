322d1e783a0c42843e887b6fd733e6089488453f
Aleksey Kostyunin <kostunin-ag@mail.ru>
2023-12-09 15:27:14 +0500

    Java add dz

--- Java/Lesson_6/Printer.java
+++ Java/Lesson_6/Printer.java
@@ -1,26 +1,44 @@
 package education_gb.Java.Lesson_6;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 
 class PhoneBook {
+
     private static HashMap<String, ArrayList<Integer>> phoneBook = new HashMap<>();
 
     public void add(String name, Integer phoneNum) {
-      ArrayList<Integer> phoneNums = phoneBook.getOrDefault(name, new ArrayList<>());
+        ArrayList<Integer> phoneNums = phoneBook.getOrDefault(name, new ArrayList<>());
         phoneNums.add(phoneNum);
         phoneBook.put(name, phoneNums);
     }
 
     public ArrayList<Integer> find(String name) {
         return phoneBook.getOrDefault(name, new ArrayList<>());
-
     }
 
     public static HashMap<String, ArrayList<Integer>> getPhoneBook() {
-return phoneBook;
-
+        return phoneBook;
     }
+
+    public static void printPhoheBookSortOnDec(){
+        ArrayList<Map.Entry<String, ArrayList>> list = new ArrayList(phoneBook.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, ArrayList>>() {
+            @Override
+            public int compare(Map.Entry<String, ArrayList> o1, Map.Entry<String, ArrayList> o2){
+                return o2.getValue().size() - o1.getValue().size();
+            }
+        });
+
+        for(Map.Entry<String, ArrayList> entry: list)   {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+    }
+
+
 }
 
 public class Printer {
@@ -47,8 +65,8 @@ public class Printer {
         myPhoneBook.add(name1, phone2);
         myPhoneBook.add(name2, phone2);
 
-        System.out.println(myPhoneBook.find(name1));
-        System.out.println(PhoneBook.getPhoneBook());
-        System.out.println(myPhoneBook.find("Me"));
+        // System.out.println(PhoneBook.getPhoneBook());
+        PhoneBook.printPhoheBookSortOnDec();
+        
     }
 }
